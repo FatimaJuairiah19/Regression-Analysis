0471e957d1c87e4bfcbb669a7c8359cd40d7142f
FatimaJuairiah19 <fatimajuairiah19@gmail.com>
2021-11-16 19:42:32 +0600

    Add project files.

---
+++ RegressionAnalysis/SalaryData.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.ML.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RegressionAnalysis
+{
+    class SalaryData
+    {
+        [LoadColumn(0)]
+        public float YearsExperience;
+
+        [LoadColumn(1), ColumnName("Label")]
+        public float Salary;
+    }
+}
---
+++ RegressionAnalysis/SalaryPrediction.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.ML.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RegressionAnalysis
+{
+    class SalaryPrediction
+    {
+        [ColumnName("Score")]
+        public float PredictedSalary { get; set; }
+
+    }
+}
